9cd5242196c7ded5529a82be5be0d0592fe5a7c9
efrencav <efren@abstract-creative.com>
2019-06-07 19:21:53 -0500

    jwt token

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const {
     check,
     validationResult
@@ -40,7 +44,7 @@ router.post('/', [
                 email
             });
             if (user) {
-                res.status(400).json({
+                return res.status(400).json({
                     errors: [{
                         msg: 'User already exists'
                     }]
@@ -48,12 +52,43 @@ router.post('/', [
             }
 
             // Get users gravatar
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            });
 
-            // Encrypt password using bcrypt
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            });
 
-            // Return jsonwebtoken
+            const salt = await bcrypt.genSalt(10);
 
-            res.send('User route');
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(payload,
+                config.get('jwtSecret'), {
+                    expiresIn: 3600000
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({
+                        token
+                    });
+                }
+            );
 
         } catch (err) {
             console.error(err.message);
