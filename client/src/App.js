a89a8753d9c4c0eb25a5f88a07b585845966c528
efrencav <efren@abstract-creative.com>
2019-06-09 12:03:54 -0500

    new users registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React, { Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Router exact path="/register" component={Register} />
+          <Router exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
